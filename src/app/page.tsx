548a558b9d92becbca52c186370eec97bb66f903
Sergey Ovchinnikov <s.ovchinnikov@challenge-insure.ru>
2025-09-09 20:48:19 +0700

    fix: 💄 Правки шаблона

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import Page from "@/components/layout/Page/Page";
+import Page from '@/components/layout/Page/Page';
 
 
 const HomePage = (): React.ReactNode => {
   return (
     <Page>
       <h1>Page</h1>
+      <h2>Заголовок 2</h2>
+      <h3>Заголовок 3</h3>
+      <h4>Заголовок 4</h4>
     </Page>
   );
 };
